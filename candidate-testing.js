c100d37945cf97e26ccef9bd9a8348f0605aa72c
badhwar <prernabadhwar@gmail.com>
2021-02-20 23:02:30 +0000

    Removed Trailing Spaces in Questions and changed case Sensitivity for one of the answers

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,16 +12,16 @@ let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 
 let questions = [
-  "Who was the first American woman in space? ",
-  "True or false: 5000 meters = 5 kilometers. ",
-  "(5 + 3)/2 * 10 = ? ",
-  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
-  "What is the minimum crew size for the ISS? "
+  "Who was the first American woman in space?",
+  "True or false: 5000 meters = 5 kilometers.",
+  "(5 + 3)/2 * 10 = ?",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?",
+  "What is the minimum crew size for the ISS?"
   ];
 
 let correctAnswers = [
   "Sally Ride",
-  "True",
+  "true",
   "40",
   "Trajectory",
   "3"
@@ -38,7 +38,7 @@ function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
     for (let i = 0; i < questions.length; i++) {
-      candidateAnswers.push(input.question("\n"+questions[i]));
+      candidateAnswers.push(input.question("\n"+questions[i]+" "));
   }
   
 }
